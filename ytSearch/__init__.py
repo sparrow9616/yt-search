1f8a37fd1f0f1a6b92f84a6396cbc90bd8b5e901
sparrow9616 <108956698+sparrow9616@users.noreply.github.com>
2025-04-08 10:13:51 +0530

    first commit

---
+++ ytSearch/__init__.py
@@ -0,0 +1,8 @@
+from ytSearch.search import *
+from ytSearch.core.constants import *
+
+
+__title__        = 'youtube-search-python'
+__version__      = '1.6.2'
+__author__       = 'alexmercerind'
+__license__      = 'MIT'
