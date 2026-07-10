d727c7006c4e1bf1194419e855ad91e6d0632017
sparrow9616 <gamingleague09@gmail.com>
2026-07-10 10:10:41 +0530

    Add PyPI publish workflow and bump to 1.1.0
    Publishes on GitHub release via PyPI trusted publishing (OIDC), so no
    API token is stored in the repository.

--- ytSearch/__init__.py
+++ ytSearch/__init__.py
@@ -5,6 +5,6 @@ from ytSearch.core.constants import *
 
 
 __title__        = 'ytSearch'
-__version__      = '1.0.0'
+__version__      = '1.1.0'
 __author__       = 'sparrow9616'
 __license__      = 'MIT'
